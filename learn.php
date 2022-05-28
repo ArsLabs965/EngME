d7dee8a5d7e4d9dbe6d02f13459c2c0d40f2ac15
ArsLabs965 <arsenov965@gmail.com>
2022-05-28 23:01:31 +0300

    Первая рабочая модель

--- learn.php
+++ learn.php
@@ -2,8 +2,8 @@
       session_start();
       include "settings/rootway.php";
   if($_SESSION['user'] == NULL){
-      header(rootway());
-      exit();
+      //header(rootway());
+      //exit();
   }
   
   //Подключение к Базе данных
@@ -58,24 +58,116 @@
         <h1><?php echo $ki['name']; ?></h1>
         <br><br>
         <div class="line">
+            <p id="counter"></p><br>
             Как это переводится?
         <div class="word">
-            Тестовое слово
+            
         </div>
         <br>
         <br>
         Варианты ответа
-        <div class="var">
-            This
+        <div class="var" id="v1" onclick="cl(1)">
+            
         </div>
-        <div class="var">
-           that
+        <div class="var" id="v2" onclick="cl(2)">
+           
         </div>
-        <div class="var">
-            or it
+        <div class="var" id="v3" onclick="cl(3)">
+            
         </div>
         </div>
    </div>
+<script src="jquery.js"></script>
+<script>
+    var en = [];
+    var ru = [];
+    var asked = [];
+    var ng = 0;
+    var goed = 0;
+    var tr = 0;
+    var canclick = 1;
 
+    <?php
+ $colvo = 0;
+ $query = mysqli_query($connection, "SELECT * FROM `words` WHERE `kit` = '$kit'");
+ while(($wd = mysqli_fetch_assoc($query))){
+     ?>
+    en[<?php echo $colvo; ?>] = '<?php echo $wd['eng']; ?>';
+    ru[<?php echo $colvo; ?>] = '<?php echo $wd['rus']; ?>';
+     <?php
+$colvo++;
+ }
+    ?>
+
+
+
+    if(en.length < 3){
+        $(".line").html("Слишком мало слов! Минимум 3. Добавьте недостающие слова в меню 'Настройки слов'");
+    }else{
+        newgen();
+    }
+
+    function newgen(){
+        if(goed == en.length){
+            $(".line").html("Вы прошли набор слов!");
+            return 0;
+        }
+        
+        do{
+            ng = rand(en.length);
+        }while(asked[ng] == 1)
+        $(".word").html(ru[ng]);
+        $("#counter").html(goed + '/' + en.length);
+        var rp = rand(3) + 1;
+        tr = rp;
+        var olds = -1;
+        $("#v" + rp).html(en[ng]);
+        $("#v1").css('background-color', 'rgb(253, 123, 47)');
+        $("#v2").css('background-color', 'rgb(253, 123, 47)');
+        $("#v3").css('background-color', 'rgb(253, 123, 47)');
+        canclick = 1;
+        for(var i = 1; i < 4; i++){
+            if(i == rp){
+                continue;
+            }
+            var rs;
+            do{
+                rs = rand(en.length);
+                if(olds != -1){
+                if(olds == rs){
+                    rs = ng;
+                }
+            }else{
+              
+            }
+           
+            }while(rs == ng)
+            olds = rs;
+           
+            $("#v" + i).html(en[rs]);
+        }
+    }
+
+    function cl(nu){
+        if(canclick){
+
+        
+        if(nu == tr){
+            asked[ng] = 1;
+            goed++;
+            $("#v" + tr).css('background-color', 'Green');
+        }else{
+            $("#v" + tr).css('background-color', 'Green');
+            $("#v" + nu).css('background-color', 'Red');
+        }
+        canclick = 0;
+        setTimeout(newgen, 2000);
+    }
+    }
+
+    function rand(max) {
+  return Math.floor(Math.random() * max);
+}
+</script>
 </body>
 </html>
